6e1e806ea7afd62543816313ac03cba042d8e7f4
lukaszfalkowski <lukaszfalkowski@users.noreply.github.com>
2025-02-09 20:18:33 +0100

    Update draw-on-map.js

--- draw-on-map.js
+++ draw-on-map.js
@@ -1,19 +1,19 @@
 var map = new mapboxgl.Map({
-  container: 'map',
-  style: {
-    version: 8,
-    sources: {
-      osm: {
-        type: 'raster',
-        tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
-        tileSize: 256,
-        attribution: 'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>, under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>'
-      }
-    },
-    layers: [{
-      id: 'osm',
-      type: 'raster',
-      source: 'osm',
-    }],
-  }
+	container: 'map-container',
+	style: {
+		version: 8,
+		sources: {
+			osm: {
+				type: 'raster',
+				tiles: ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
+				tileSize: 256,
+				attribution: 'Map tiles by <a target="_top" rel="noopener" href="https://tile.openstreetmap.org/">OpenStreetMap tile servers</a>, under the <a target="_top" rel="noopener" href="https://operations.osmfoundation.org/policies/tiles/">tile usage policy</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>'
+			}
+		},
+		layers: [{
+			id: 'osm',
+			type: 'raster',
+			source: 'osm',
+		}],
+	}
 });
